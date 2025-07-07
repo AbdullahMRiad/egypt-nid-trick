5f1251880cfba32e385986a5ff35602405f9aa91
AbdullahMRiad <abdullahmriad@gmail.com>
2025-07-07 03:52:51 +0300

    Add vite-plugin-gh-pages for deployment and update package.json scripts
    - Added "vite-plugin-gh-pages" to dependencies in package.json.
    - Updated scripts in package.json to include a deploy command.
    - Configured vite.config.ts to use ghPages plugin and set base path.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { ghPages } from 'vite-plugin-gh-pages'
+
 export default defineConfig({
+  base: "/egypt-nid-trick/",
   plugins: [
     tailwindcss(),
+    ghPages()
   ],
 })
