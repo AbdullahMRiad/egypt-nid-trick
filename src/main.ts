4fba958aa1e017ba633bb54987bbb1df4b04d7c4
AbdullahMRiad <abdullahmriad@gmail.com>
2025-07-07 03:54:28 +0300

    Remove unused type import and variable declaration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import Calculate from "./ts/calculate"
-import type { OutputData } from "./ts/types"
 
 export const dobLabel:         HTMLSpanElement   = document.getElementById("dob")    as HTMLSpanElement
 export const governorateLabel: HTMLSpanElement   = document.getElementById("gov")    as HTMLSpanElement
@@ -7,6 +6,4 @@ export const genderLabel:      HTMLSpanElement   = document.getElementById("gen"
        const nationalIdInput:  HTMLInputElement  = document.getElementById("id-number")     as HTMLInputElement
        const submitButton:     HTMLButtonElement = document.getElementById("submit") as HTMLButtonElement
 
-let   data: OutputData
-
-submitButton.addEventListener("click", () => data = Calculate(nationalIdInput.value.toString()))
+submitButton.addEventListener("click", () => Calculate(nationalIdInput.value.toString()))
